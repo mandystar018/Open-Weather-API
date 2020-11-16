587a94227ffeea9e2fc4bcbffbaf46974caeb95b
Amanda Otero <mandystar018@gmail.com>
2020-11-16 11:47:35 -0800

    Add a constructor and test tempF constructor, purposufully fail
    Co-authored-by: Kevin Kirkley <kevinmkirkley@gmail.com>

--- _test_/temp.test.js
+++ _test_/temp.test.js
@@ -9,5 +9,8 @@ describe('Temp', () => {
 
     test('Should create a class with a parameter fro temp in Kelvin', () => {
       expect(temp.tempK).toEqual(283.15);
-    })
+    });
+    test('Should convert temp Kelvin into temp Fahrenheit.', () => {
+      expect(temp.tempF).toEqual(51);
+    });
 });
--- src/js/temp.js
+++ src/js/temp.js
@@ -1,6 +1,6 @@
 export default class Temp {
   constructor(tempK) {
     this.tempK = tempK;
-    // this.tempF = Math.round((tempK - 273.15)*(9/5)+32);
+    this.tempF = Math.round((tempK - 273.15)*(9/5)+32);
   }
 }
