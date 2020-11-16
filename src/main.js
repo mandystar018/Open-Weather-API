c8d3a4731dad2211527f80be0f31cc1848c34a95
Amanda Otero <mandystar018@gmail.com>
2020-11-16 14:19:45 -0800

    Add another API call so the user can input their zipcode
    Co-authored-by: Kevin Kirkley <kevinmkirkley@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,14 +2,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Temp from './js/temp.js';
 
 
 $('#weatherLocation').click(function() {
   const city = $('#location').val();
+  const zipCode = $('#zip-code').val();
   $('#location').val("");
+  $('#zip-code').val("");
 
   let request = new XMLHttpRequest();
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+  const urlZip =`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},&appid=${process.env.API_KEY}`
 
   request.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
@@ -20,10 +24,14 @@ $('#weatherLocation').click(function() {
 
   request.open("GET", url, true);
   request.send();
-
+  request.open("GET", urlZip, true)
+  request.send();
+  
   function getElements(response) {
+    let temp = new Temp(response.main.temp);
+    $('.showLocation').text(`Weather report for ${response.name}`)
     $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    $('.showTemp').text(`The temperature in Fahrenheit is ${temp.tempF} degrees.`);
     $('.showCloudCover').text(`The current cloud cover in ${city} is ${response.weather[0].description} of ${response.clouds.all}% coverage.`)
   }
 });
